bb8cfdee38fdc6b67b2f54d41825d43d7a99ce1a
Xu-X <2235975386@qq.com>
2019-07-09 21:04:55 +0800

    贾梦涵大傻逼

---
+++ wine-production-xu/src/main/java/com/jk/WineProductionXuApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineProductionXuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineProductionXuApplication.class, args);
+    }
+
+}
