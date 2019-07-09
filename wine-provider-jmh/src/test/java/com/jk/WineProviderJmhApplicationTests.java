0cb9fb698d06571236c562a6d443b2f62bb20eaa
jiamHan <2486381637@qq.com>
2019-07-09 20:18:10 +0800

    创建声å生产者和feign

---
+++ wine-provider-jmh/src/test/java/com/jk/WineProviderJmhApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WineProviderJmhApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
