b575aaec3e10e63b0bbbcc470e8ef1c512ff2624
dfp777 <563680507@qq.com>
2019-07-11 10:04:44 +0800

    test

--- wine-feign-dfp/src/main/java/com/jk/controller/DsController.java
+++ wine-feign-dfp/src/main/java/com/jk/controller/DsController.java
@@ -1,5 +1,12 @@
 package com.jk.controller;
 
+import com.jk.service.DsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
 /**
  * @Classname DsController
  * @Description TODO
@@ -7,4 +14,13 @@ package com.jk.controller;
  * @Created by dfp777
  */
 public class DsController {
+    @Autowired
+    private DsService DsService;
+
+    @GetMapping("findchuizi")
+    public HashMap<String,Object> findNews(@RequestParam Integer start, @RequestParam Integer pageSize){
+
+        return DsService.findchuizi(start,pageSize);
+    }
+
 }
