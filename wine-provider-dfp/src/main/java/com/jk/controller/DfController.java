dc4c7583f00219679d57e0826c03de8cf70d7fa9
dfp777 <563680507@qq.com>
2019-07-11 10:04:20 +0800

    test

--- wine-provider-dfp/src/main/java/com/jk/controller/DfController.java
+++ wine-provider-dfp/src/main/java/com/jk/controller/DfController.java
@@ -1,10 +1,28 @@
 package com.jk.controller;
 
+import com.jk.service.DfServiceImpl;
+import com.jk.service.GoodsServiceApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
 /**
  * @Classname DfController
  * @Description TODO
  * @Date 2019/7/10 20:03
  * @Created by dfp777
  */
-public class DfController {
+@RestController
+public class DfController implements GoodsServiceApi {
+    @Autowired
+    private DfServiceImpl newsService;
+
+    @Override
+    @GetMapping("findNews")
+    public HashMap<String, Object> findchuizi(@RequestParam Integer start, @RequestParam Integer pageSize) {
+        return newsService.findchuizi(start,pageSize);
+    }
 }
--- wine-provider-dfp/src/main/java/com/jk/service/DfServiceImpl.java
+++ wine-provider-dfp/src/main/java/com/jk/service/DfServiceImpl.java
@@ -1,10 +1,31 @@
 package com.jk.service;
 
+import com.jk.mapper.DsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 /**
  * @Classname DfServiceImpl
  * @Description TODO
  * @Date 2019/7/10 20:08
  * @Created by dfp777
  */
-public class DfServiceImpl {
+@Service
+public class DfServiceImpl  implements  GoodsServiceApi{
+    @Autowired
+    private DsMapper newsMapper;
+
+    @Override
+    public HashMap<String, Object> findchuizi(Integer start, Integer rows) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        Integer  total = newsMapper.queryNewsCount();
+        List<LinkedHashMap<String,Object>> find = newsMapper.findNews(start,rows);
+        hashMap.put("total",total);
+        hashMap.put("rows",find);
+        return hashMap;
+    }
 }
