85b1bbf82d6f784933371f271225a49aaacdde8a
jiamHan <2486381637@qq.com>
2019-07-15 09:49:26 +0800

    新增user-coupon表

---
+++ wine-provider-jmh/src/main/java/com/jk/mapper/WineMapper.java
@@ -0,0 +1,16 @@
+package com.jk.mapper;
+
+import com.jk.model.User_Coupon;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @Classname WineMapper
+ * @Description TODO
+ * @Date 2019/7/14 16:51
+ * @Created by jmh
+ */
+@Mapper
+public interface WineMapper {
+    void addReceive(User_Coupon userCoupon);
+}
--- wine-web-ui/src/main/java/com/jk/controller/CoresController.java
+++ wine-web-ui/src/main/java/com/jk/controller/CoresController.java
@@ -1,6 +1,8 @@
 package com.jk.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @Classname 曹龙启
@@ -8,6 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/7/9 17:32
  * @Created by ${jmh}
  */
-@RestController
+@Controller
 public class CoresController {
+
+    //跳转领券中心
+    @RequestMapping("toShow")
+    public String toShow(){
+        return "activity-1981";
+    }
+
+    //跳转我的优惠券
+    @RequestMapping("toShow2")
+    public String toShow2(){
+        return "my_coupon-11";
+    }
+
 }
