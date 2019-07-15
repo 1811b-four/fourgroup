be85351bc703e0d7427675089b2c2eabcbd82476
Xu-X <2235975386@qq.com>
2019-07-15 11:56:22 +0800

    更新

---
+++ wine-service-Api/src/main/java/com/jk/service/shoppingcar/CarServicePublic.java
@@ -0,0 +1,10 @@
+package com.jk.service.shoppingcar;
+
+/**
+ * @Classname CarServicePublic
+ * @Description TODO
+ * @Date 2019-07-14 11:05
+ * @Created by 谷凌杰
+ */
+public interface CarServicePublic {
+}
