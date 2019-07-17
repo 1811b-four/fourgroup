34ffd7b53cca513538b26d0bf55a13c111bcb9b6
jiamHan <2486381637@qq.com>
2019-07-17 10:33:02 +0800

    解决项目冲突

--- wine-service-Api/src/main/java/com/jk/service/TreeService.java
+++ wine-service-Api/src/main/java/com/jk/service/TreeService.java
@@ -4,7 +4,6 @@ import com.jk.utils.MenuTree;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
