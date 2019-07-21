f4cca86fceda81432cb2fb07ee88d162cddb97e9
qilong666 <904533914@qq.com>
2019-07-21 16:29:33 +0800

    es1

---
+++ wine-consumer-wy/src/main/java/com/jk/controller/ElasticController.java
@@ -0,0 +1,13 @@
+package com.jk.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname 曹龙启
+ * @Description TODO
+ * @Date 2019/7/21 16:28
+ * @Created by ${jmh}
+ */
+@RestController
+public class ElasticController {
+}
