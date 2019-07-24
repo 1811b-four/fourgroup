bbca803099c2e05d141ec81ffd5ac02048ca6fd6
jiamHan <2486381637@qq.com>
2019-07-24 23:35:59 +0800

    修改API

---
+++ wine-service-Api/src/main/java/com/jk/service/Coupon.java
@@ -0,0 +1,35 @@
+package com.jk.service;
+
+import com.jk.model.User_Coupon;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Classname Coupon
+ * @Description TODO
+ * @Date 2019/7/24 23:21
+ * @Created by jmh
+ */
+public interface Coupon {
+
+    @PostMapping("receive")
+    Map<String, Object> addReceive(@RequestParam(value = "couponId", required = true) Integer couponId, User_Coupon userCoupon);
+
+    @PostMapping("getCouponNotUsed")
+    HashMap<String, Object> getCouponNotUsed(@RequestParam(value = "start",required = true) Integer start, @RequestParam(value = "pageSize",required = true) Integer pageSize);
+
+    @PostMapping("getCouponUsed")
+    HashMap<String, Object> getCouponUsed(@RequestParam(value = "start",required = true) Integer start, @RequestParam(value = "pageSize",required = true) Integer pageSize);
+
+    @PostMapping("getCouponExpired")
+    HashMap<String, Object> getCouponExpired(@RequestParam(value = "start",required = true) Integer start, @RequestParam(value = "pageSize",required = true) Integer pageSize);
+
+    @PostMapping("getOrder")
+    HashMap<String, Object> getOrder(@RequestParam(value = "start",required = true) Integer start, @RequestParam(value = "pageSize",required = true) Integer pageSize);
+
+
+
+}
